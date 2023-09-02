6cc0b73211f49ca3f5072d3423195ee773d8bc00
Florian Eckert <florian.eckert@unistra.fr>
2023-09-02 19:10:16 +0200

    Strategie naiive

--- Le_golf/Balle.cs
+++ Le_golf/Balle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using DiceNamespace;
@@ -21,8 +22,81 @@ namespace BalleNamespace
             _dimention = dimention;
             _coordonee = new int[dimention];
 
+
+            for (int i = 0; i < dimention; i++)
+            {
+                _coordonee[i] = 1000;
+            }
+
         }
-)
+
+        public bool Deplacer(int longueur, int axe=0)
+        {
+            if (axe < 0 || axe >= _dimention)
+            {
+                throw new ArgumentException($"USAGE : La dimention {axe} n'existe pas");
+            }
+            if (_coordonee[axe] == 0)
+            {
+                return true;
+            }
+
+            if (_coordonee[axe] < 0)
+            {
+                _coordonee[axe] += longueur;
+            }
+            else
+            {
+                _coordonee[axe] -= longueur;
+            }
+
+            return _coordonee[axe] == 0;
+        }
+
+        public bool StrategiNaive(int longueur)
+        {
+            int bestAxe = 0;
+            int minDist = int.MaxValue;
+            for (int i = 0; i < _coordonee.Length ; i++)
+            {
+                if (_coordonee[i] == 0)
+                {
+                    continue;
+                }
+                else if (_coordonee[i] == longueur)
+                {
+                    bestAxe = i;
+                    break;
+                }
+                else if (_coordonee[i] > 0)
+                {
+                    
+                    if (Math.Abs(_coordonee[i] - longueur) < minDist)
+                    {
+                        bestAxe = i;
+                    }
+                }
+                else
+                {
+                    if (Math.Abs(_coordonee[i] + longueur) < minDist)
+                    {
+                        bestAxe = i;
+                    }
+                }
+            }
+
+            Deplacer(longueur, bestAxe);
+
+            for (int d = 0; d < _coordonee.Length ; d++)
+            {
+                if (_coordonee[d] != 0) 
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
 
     }
 }
--- Le_golf/Program.cs
+++ Le_golf/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BalleNamespace;
 using DiceNamespace;
 
 namespace Le_golf
@@ -12,6 +13,30 @@ namespace Le_golf
         static void Main(string[] args)
         {
             DeGeneral de = new DeGeneral();
+
+            int nbBalle = 1000;
+            int nbDimention = 4;
+
+            Balle[,] balles = new Balle[nbBalle,nbDimention];
+            decimal[] meanCoup = new decimal[nbDimention];
+            
+            for (int d = 0; d < nbDimention; d++)
+            {
+                meanCoup[d] = 0;
+                for (int i = 0; i< nbBalle; i++)
+                {
+                    balles[i, d] = new Balle(d+1);
+                    while (!balles[i,d].StrategiNaive(de.roll()))
+                    {
+                        meanCoup[d]++;
+                    }
+                }
+                meanCoup[d] /= nbBalle;
+                Console.WriteLine($"Nombre de coup moyen : {meanCoup[d]} pour {d} dimention avec D6");
+                //Console.WriteLine($"Performance : {meanCoup[d] / (decimal)Math.Pow(1000 * 1000, 1/2)}");
+            }
+            
+            Console.ReadKey();
         }
     }
 }
